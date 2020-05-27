efa914990d4e0166a8b9c58890c745f0c5bc255a
José Carlos Santos <carlos.avellar@gmail.com>
2020-05-27 11:01:29 -0300

    Erro da autocritica

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -3,28 +3,13 @@ const { catchAsync } = require('./../utils/catchAsync');
 const APIfeatures = require('./../utils/APIfeatures');
 
 exports.getAllTours = catchAsync(async (req, res, next) => {
-  const features = new APIfeatures(Tour.find(), req.query)
-    .filtered()
-    .sortBy()
-    .paginate()
-    .limitFields();
+  const features = new APIfeatures(Tour.find(), req.query).filtered();
 
   const tours = await features.query;
   res.status(200).json({
     status: 'Sucsess',
-    results: tours.length,
     data: {
       tours,
-    },
-  });
-});
-
-exports.createTour = catchAsync(async (req, res, next) => {
-  const tour = Tour.create(req.body);
-  res.status(201).json({
-    status: 'Created',
-    data: {
-      tour,
     },
   });
 });
