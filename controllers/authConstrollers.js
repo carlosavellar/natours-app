946ae791c3af38bd61b6bace9eef0d5240616fcb
José Carlos Santos <carlos.avellar@gmail.com>
2020-06-07 18:19:55 -0300

    signin login

--- controllers/authConstrollers.js
+++ controllers/authConstrollers.js
@@ -1,9 +1,17 @@
 const User = require('./../models/userModel');
 const { catchAsync } = require('./../utils/catchAsync');
 const AppError = require('./../utils/AppError');
+const jwt = require('jsonwebtoken');
+const signInToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
+  const token = signInToken(newUser._id);
+
   res.status(201).json({
     status: 'Success',
     data: {
@@ -27,9 +35,10 @@ exports.login = catchAsync(async (req, res, next) => {
   if (!user || !correct) {
     return new AppError('Incorrect password', 400);
   }
+  const token = signInToken(user._id);
 
   res.status(201).json({
     status: 'Success',
-    loggedUser: user,
+    token,
   });
 });
