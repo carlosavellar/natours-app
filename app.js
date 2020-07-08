b2ffa177a30ae00e96bcffcd589ee39a7c48ecad
José Carlos Santos <jcarlos@Joses-MacBook-Pro.local>
2020-07-08 10:34:00 -0300

    tour Controller Complete

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
-const AppError = require('./utils/AppError');
-const errorControler = require('./controllers/errorController');
 const app = express();
 
 if (process.env.NODE_ENV === 'development') {
@@ -12,21 +10,5 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
-
-app.all('*', (req, res, next) => {
-  // const err = new Error(`This path doesn't exist ${req.originalUrl}`, 404);
-  // err.statuCode = err.statusCode || 400;
-  // err.status = err.status || 'Fail';
-  next(new AppError(`This path doesn't exist ${req.originalUrl}`, 404));
-});
-
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'Error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
 
 module.exports = app;
