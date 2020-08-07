d556e8ac01ffa1210f5aa5c38323e338d6913120
carlosavellar <carlos.avellar@gmail.com>
2020-08-07 17:40:54 -0300

    auth: signUp user ok

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 
-// const userRouter = require('./routes/userRoutes');
+const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
-// app.use('/api/v1/users', userRouter);
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`This path doesn't exist ${req.originalUrl}`));
